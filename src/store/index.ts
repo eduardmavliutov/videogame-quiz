d8ffa242465e67f4bafcccc2ff1567085b985ed2
Eduard <fox11-best@yandex.ru>
2021-05-08 10:15:17 +0300

    feat: removed active quiz question vuex module

--- src/store/index.ts
+++ src/store/index.ts
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import { quiz } from '@/store/quiz/index'
 import { popup } from '@/store/popup/index'
-import { activeQuizQuestion } from '@/store/activeQuizQuestion/index'
 import { auth } from '@/store/auth/index'
 import { user } from '@/store/user/index'
 import createPersistedState from 'vuex-persistedstate'
@@ -18,7 +17,6 @@ export default new Vuex.Store({
   modules: {
     quiz,
     popup,
-    activeQuizQuestion,
     auth,
     user
   },
