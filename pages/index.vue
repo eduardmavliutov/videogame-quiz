64dff1a70ea1a7b56730c60db2de4333b723014e
Eduard <fox11-best@yandex.ru>
2021-08-18 21:48:49 +0300

    feat: added special page transitions for moving between quiz and quiz question page (ActiveQuestionPage component)

--- pages/index.vue
+++ pages/index.vue
@@ -23,19 +23,146 @@ import { Quiz } from '@/types/store/quiz/quiz.interface'
   }
 })
 export default class Home extends Vue {
-  private quizes!: { [key: string]: Quiz }
-  private loading = true
+  // private quizes!: { [key: string]: Quiz }
+  // ! временно пока доступ в firebase не будет возобновлен
+  private quizes: { [key: string]: Quiz } = {
+    '324rfsd': {
+      id: '324rfsd',
+      title: 'PS4 exclusives',
+      image: {
+        src: require('@/assets/images/quizes/ps4-exclusives/ps4-exclusives.jpeg'),
+        alt: 'ps4-exclusives'
+      },
+      questions: [
+        {
+          rightAnswer: 'god of war',
+          image: {
+            src: require('@/assets/images/quizes/ps4-exclusives/god-of-war.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          imagePreview: {
+            src: require('@/assets/images/quizes/ps4-exclusives/god-of-war-thumb.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          letterPool: [
+            { value: 'f' },
+            { value: 'g' },
+            { value: 'o' },
+            { value: 'd' },
+            { value: 'w' },
+            { value: 'o' },
+            { value: 'f' },
+            { value: 'a' },
+            { value: '6' },
+            { value: 'r' },
+            { value: 'n' },
+            { value: 'm' },
+            { value: 'u' },
+            { value: '5' }
+          ],
+          openedLetters: [
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: ' ' },
+            { value: '' },
+            { value: '' },
+            { value: ' ' },
+            { value: '' },
+            { value: '' },
+            { value: '' }
+          ]
+        },
+        {
+          rightAnswer: 'uncharted 4',
+          image: {
+            src: require('@/assets/images/quizes/ps4-exclusives/uncharted-4.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          imagePreview: {
+            src: require('@/assets/images/quizes/ps4-exclusives/uncharted-4-thumb.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          letterPool: [
+            { value: 'c' },
+            { value: 'h' },
+            { value: 'u' },
+            { value: 'a' },
+            { value: 'n' },
+            { value: '4' },
+            { value: 'd' },
+            { value: 'n' },
+            { value: 'e' },
+            { value: 't' },
+            { value: 'r' }
+          ],
+          openedLetters: [
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: ' ' },
+            { value: '' }
+          ]
+        },
+        {
+          rightAnswer: 'persona 5',
+          image: {
+            src: require('@/assets/images/quizes/ps4-exclusives/persona-5.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          imagePreview: {
+            src: require('@/assets/images/quizes/ps4-exclusives/persona-5-thumb.jpeg'),
+            alt: 'quiz-picture-alt'
+          },
+          letterPool: [
+            { value: 'e' },
+            { value: 'p' },
+            { value: 'r' },
+            { value: 'o' },
+            { value: 's' },
+            { value: 'n' },
+            { value: '5' },
+            { value: '3' },
+            { value: 'a' },
+            { value: 'a' },
+            { value: 's' },
+            { value: 'a' },
+            { value: 'w' },
+            { value: 'm' }
+          ],
+          openedLetters: [
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: '' },
+            { value: ' ' },
+            { value: '' }
+          ]
+        }
+      ],
+    }
+  }
+  private loading = false
 
   async created () {
-    try {
-      this.loading = true
-      const snapshot = await this.$fire.database.ref('/quizes').once('value')
-      this.quizes = snapshot.val()
-    } catch (error) {
-      console.log(error)
-    } finally {
-      this.loading = false
-    }
+    // try {
+    //   this.loading = true
+    //   const snapshot = await this.$fire.database.ref('/quizes').once('value')
+    //   this.quizes = snapshot.val()
+    // } catch (error) {
+    //   console.log(error)
+    // } finally {
+    //   this.loading = false
+    // }
   }
 }
 </script>
--- views/QuizPage/QuizPage.vue
+++ views/QuizPage/QuizPage.vue
@@ -12,7 +12,7 @@
         @click.native="quizQuestionClickHandler(index)"
       />
     </v-grid>
-    <router-view/>
+    <router-view />
   </v-page>
 </template>
 <script lang="ts">
@@ -34,21 +34,146 @@ const quizModule = namespace('quiz')
     VPage
   },
   async asyncData ({ $fire, route }) {
-    try {
-      const quizId = route.params.quizId
-      const snapshot = await $fire.database.ref(`/quizes/${quizId}`).once('value')
-      const quiz: Quiz = snapshot.val()
-      return {
-        quiz
-      }
-    } catch (error) {
-      console.log(error)
-    }
+    // try {
+    //   const quizId = route.params.quizId
+    //   const snapshot = await $fire.database.ref(`/quizes/${quizId}`).once('value')
+    //   const quiz: Quiz = snapshot.val()
+    //   return {
+    //     quiz
+    //   }
+    // } catch (error) {
+    //   console.log(error)
+    // }
   }
 })
 export default class QuizPage extends Vue {
   // @quizModule.Getter('quiz') quiz!: (quizId: string) => Quiz
-  private quiz!: Quiz
+  // private quiz!: Quiz
+  // ! временно пока доступ в firebase не будет возобновлен
+  private quiz = {
+    id: '324rfsd',
+    title: 'PS4 exclusives',
+    image: {
+      src: require('@/assets/images/quizes/ps4-exclusives/ps4-exclusives.jpeg'),
+      alt: 'ps4-exclusives'
+    },
+    questions: [
+      {
+        rightAnswer: 'god of war',
+        image: {
+          src: require('@/assets/images/quizes/ps4-exclusives/god-of-war.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        imagePreview: {
+          src: require('@/assets/images/quizes/ps4-exclusives/god-of-war-thumb.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        letterPool: [
+          { value: 'f' },
+          { value: 'g' },
+          { value: 'o' },
+          { value: 'd' },
+          { value: 'w' },
+          { value: 'o' },
+          { value: 'f' },
+          { value: 'a' },
+          { value: '6' },
+          { value: 'r' },
+          { value: 'n' },
+          { value: 'm' },
+          { value: 'u' },
+          { value: '5' }
+        ],
+        openedLetters: [
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: ' ' },
+          { value: '' },
+          { value: '' },
+          { value: ' ' },
+          { value: '' },
+          { value: '' },
+          { value: '' }
+        ]
+      },
+      {
+        rightAnswer: 'uncharted 4',
+        image: {
+          src: require('@/assets/images/quizes/ps4-exclusives/uncharted-4.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        imagePreview: {
+          src: require('@/assets/images/quizes/ps4-exclusives/uncharted-4-thumb.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        letterPool: [
+          { value: 'c' },
+          { value: 'h' },
+          { value: 'u' },
+          { value: 'a' },
+          { value: 'n' },
+          { value: '4' },
+          { value: 'd' },
+          { value: 'n' },
+          { value: 'e' },
+          { value: 't' },
+          { value: 'r' }
+        ],
+        openedLetters: [
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: ' ' },
+          { value: '' }
+        ]
+      },
+      {
+        rightAnswer: 'persona 5',
+        image: {
+          src: require('@/assets/images/quizes/ps4-exclusives/persona-5.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        imagePreview: {
+          src: require('@/assets/images/quizes/ps4-exclusives/persona-5-thumb.jpeg'),
+          alt: 'quiz-picture-alt'
+        },
+        letterPool: [
+          { value: 'e' },
+          { value: 'p' },
+          { value: 'r' },
+          { value: 'o' },
+          { value: 's' },
+          { value: 'n' },
+          { value: '5' },
+          { value: '3' },
+          { value: 'a' },
+          { value: 'a' },
+          { value: 's' },
+          { value: 'a' },
+          { value: 'w' },
+          { value: 'm' }
+        ],
+        openedLetters: [
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: '' },
+          { value: ' ' },
+          { value: '' }
+        ]
+      }
+    ],
+  }
 
   // get computedQuiz (): Quiz {
   //   return this.quiz(`${this.$route.params.quizId}`)
